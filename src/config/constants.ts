83ddf871c46f405ec489a3aaa9ce2030724d871f
IgorLebedev <igorlebedev228@gmail.com>
2026-04-22 22:03:41 +0500

    add: config consts

---
+++ src/config/constants.ts
@@ -0,0 +1,6 @@
+export const NETWORK = 'mainnet';
+
+export const AFTERMATH_VALIDATOR_ADDRESS =
+  '0xd30018ec3f5ff1a3c75656abf927a87d7f0529e6dc89c7ddd1bd27ecb05e3db2';
+
+export const SAFE_GAS_AMOUNT = 1;
