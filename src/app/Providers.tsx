cb86da095ac0dfffc24fb74434cae1500aaed0b7
IgorLebedev <igorlebedev228@gmail.com>
2026-04-22 22:04:09 +0500

    add: toaster

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,10 +1,16 @@
-import { DAppKitProvider } from "@mysten/dapp-kit-react"
-import { dAppKit } from "./dapp-kit"
+import { DAppKitProvider } from '@mysten/dapp-kit-react';
+import { dAppKit } from './dapp-kit';
+import { queryClient } from '@/lib';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'sonner';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<DAppKitProvider dAppKit={dAppKit}>
-			{children}
-		</DAppKitProvider>
-	)
-}
+  return (
+    <QueryClientProvider client={queryClient}>
+      <DAppKitProvider dAppKit={dAppKit}>
+        {children}
+        <Toaster />
+      </DAppKitProvider>
+    </QueryClientProvider>
+  );
+};
