3cb302c46c0936b0dd75dc3241e92d51640e47b8
IgorLebedev <igorlebedev228@gmail.com>
2026-04-22 18:56:58 +0500

    chore: update deps, add  tanstack, shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, 'src'),
-        }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  },
+  plugins: [tailwindcss()],
+});
