31f805020b83fd591129d4692c59639bcb08c624
IgorLebedev <igorlebedev228@gmail.com>
2026-04-23 19:31:22 +0500

    feat: af sui coin type

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,4 +3,7 @@ export const NETWORK = 'mainnet';
 export const AFTERMATH_VALIDATOR_ADDRESS =
   '0xd30018ec3f5ff1a3c75656abf927a87d7f0529e6dc89c7ddd1bd27ecb05e3db2';
 
+export const AF_SUI_COIN_TYPE =
+  '0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI';
+
 export const SAFE_GAS_AMOUNT = 1;
