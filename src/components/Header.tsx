3918b3c682184225d9a9f21673f38f5a11b5f3d1
IgorLebedev <igorlebedev228@gmail.com>
2026-04-23 19:32:46 +0500

    feat: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,12 @@ import { ConnectButton } from '@mysten/dapp-kit-react/ui';
 
 export const Header = () => {
   return (
-    <div className="w-full justify-end flex p-2">
+    <div className="w-full justify-between gap-2 items-center flex px-5 py-3 border-b border-b-amber-50">
+      <a href="/">
+        <h1 className="text-white  text-balance text-xl md:text-4xl font-black">
+          Aftermath staking
+        </h1>
+      </a>
       <ConnectButton />
     </div>
   );
