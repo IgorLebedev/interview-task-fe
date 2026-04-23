f09efa96bfa10f7d2436d87bf9cd4624d9a3bc92
IgorLebedev <igorlebedev228@gmail.com>
2026-04-23 16:45:16 +0500

    chore: init react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import react from '@vitejs/plugin-react';
+import babel from 'vite-plugin-babel';
 
 export default defineConfig({
   resolve: {
@@ -8,5 +10,16 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    babel({
+      filter: /\.[jt]sx?$/,
+      include: ['src/**/*'],
+      babelConfig: {
+        presets: ['@babel/preset-typescript', ['@babel/preset-react', { runtime: 'automatic' }]],
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+  ],
 });
