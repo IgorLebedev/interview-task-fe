2ff52e44a4d69c1b0ec63b7ead42c8a7ff58ff24
IgorLebedev <igorlebedev228@gmail.com>
2026-04-23 19:31:53 +0500

    style: padding on the main page

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,11 +1,11 @@
-import { Staking } from '@/components/Staking';
+import { Staking } from '@/components/staking/';
 import { Header } from '@/components/Header';
 
 export const MainPage = () => {
   return (
     <div className="h-screen w-screen flex flex-col">
       <Header />
-      <div className="items-center flex-1 flex justify-center">
+      <div className="items-center p-4 flex-1 flex justify-center">
         <Staking />
       </div>
     </div>
